362c88297c392033654bd14a66a636bca8a24644
Rauf221 <Rauf280604@gmail.com>
2024-08-08 13:13:57 +0400

    feat: Footer Created !

---
+++ src/component/feature/Footer/footer.jsx
@@ -0,0 +1,216 @@
+import React from "react";
+import { IoMdFlame } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaPinterestP } from "react-icons/fa";
+const Footerr = () => {
+  return (
+    <>
+      <div className=" bg-[#0f0f11]">
+      <div className="h-[500px] mx-auto container w-full  justify-start flex items-center pl-20 pr-20 pt-20 pb-20 gap-16 text-white">
+        <div className="flex items-center gap-10   ">
+          <div className="flex flex-col justify-center">
+            <h1 className="text-xl  font-bold">Most Popular</h1>
+            <div className="flex mt-10">
+              <div className="">
+                <img
+                  className=" w-36 h-[80px] rounded-xl"
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2021/01/kamil-s-SuPAbuuK7f4-unsplash-1024x683.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col  gap-2  justify-between w-[400px] pl-5 ">
+                <div className="">
+                  <p className="hover:cursor-pointer transition-all duration-300 ease-in-out hover:text-[#3c3fde] text-md font-bold">
+                    Popular New Xbox Game Pass Game Being Review Bombed With
+                    “0s”
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <p className=" text-gray-400 font-semibold text-xs">Jan 14, 2021</p>
+                  <p className="text-red-500 flex items-center text-xs">
+                    <IoMdFlame />
+                    7,792 Views
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-900 h-[1px] mt-5 "></div>
+            <div className="flex mt-5">
+              <div>
+                <img
+                  className=" w-36 h-[80px]  rounded-xl"
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2021/01/jonathan-kemper-poSms2EzfNY-unsplash.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col gap-2  justify-between w-[400px] pl-5 ">
+                <div className="">
+                  <p className="hover:cursor-pointer text-md transition-all duration-300 ease-in-out hover:text-[#3c3fde] font-bold">
+                    Samsung is Developing Bright MicroLED on Displays for AR
+                    Headsets
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <p className=" text-gray-400 font-semibold text-xs ">Jan 14, 2021</p>
+                  <p className="text-red-500 flex items-center text-xs ">
+                    <IoMdFlame />
+                    6,914 Views
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-900 h-[1px] mt-5"></div>
+            <div className="flex mt-5">
+              <div>
+                <img
+                  className=" w-36 h-[80px] rounded-xl"
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2021/01/0247d91565b69ed9ddba4ec59ac97963-1.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col gap-2  justify-between w-[400px] pl-5 ">
+                <div className="">
+                  <p className="hover:cursor-pointer text-md transition-all duration-300 ease-in-out hover:text-[#3c3fde] font-bold">
+                    Warner Bros. Games Announces Mortal Kombat: Onslaught
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <p className=" text-gray-400 font-semibold text-xs  ">Jan 14, 2021</p>
+                  <p className="text-red-500 flex items-center text-xs ">
+                    <IoMdFlame />
+                    4,418 Views
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div className="flex flex-col justify-center"> 
+            <h1 className="text-xl  font-bold">Our Picks</h1>
+            <div className="flex mt-10">
+              <div className="">
+                <img
+                  className=" w-36 h-[80px] rounded-xl"
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/thisisengineering-raeng-a7A9O5htrKs-unsplash-1024x683.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col  gap-2  justify-between w-[400px] pl-5 ">
+                <div className="">
+                  <p className="hover:cursor-pointer text-md transition-all duration-300 ease-in-out hover:text-[#3c3fde] font-bold">
+                    Game Development This Week: Save On Essential Tools and More
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <p className=" text-gray-400 font-semibold text-xs">Nov 19, 2022</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-900 h-[1px] mt-5 "></div>
+            <div className="flex mt-5">
+              <div>
+                <img
+                  className=" w-36 h-[80px] rounded-xl"
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/Depositphotos_242235046_xl-2015-1-scaled.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col gap-2 justify-between w-[400px] pl-5 ">
+                <div className="">
+                  <p className="hover:cursor-pointer text-md transition-all duration-300 ease-in-out hover:text-[#3c3fde] font-bold">
+                    Is the Hyperloop Doomed? What Elon Musk’s Latest Setback
+                    Really Means
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <p className=" text-gray-400 font-semibold text-xs">Mar 10, 2022</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-900 h-[1px] mt-5"></div>
+            <div className="flex mt-5">
+              <div>
+                <img
+                  className=" w-36 h-[80px] rounded-xl"
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/remy_loz-3vVzdb8KKIw-unsplash.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col gap-2 justify-between w-[400px] pl-5 ">
+                <div className="">
+                  <p className="hover:cursor-pointer text-md transition-all duration-300 ease-in-out hover:text-[#3c3fde] font-bold">
+                    The Best Early Black Friday Deals on Gaming Laptops and
+                    Accessories
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <p className=" text-gray-400 font-semibold text-xs ">Mar 10, 2022</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-black w-[40%] h-[370px] border border-gray-900 rounded-lg flex items-center flex-col  p-10">
+          <div className="text-2xl font-bold ">
+            <h1>Subscribe to Updates</h1>
+          </div>
+          <div className="text-md  mt-5 text-center">
+            <h1>
+              Get the latest creative news from FooBar about art, design and
+              business.
+            </h1>
+          </div>
+          <div className="w-[100%] flex flex-col items-center">
+            <input
+              className="bg-[#0f0f11] pr-2 pl-2  w-[300px] mt-5 pb-3 pt-3 border border-gray-700 rounded-sm  text-center text-md"
+              type="text"
+              placeholder="Your email address.."
+            />
+            <button className="w-[300px] pb-2 pt-2 bg-blue-600 mt-5 rounded-sm">
+              SUBSCRIBE
+            </button>
+          </div>
+          <div className="flex items-baseline mt-5">
+            <input className="w-3 h-3" type="checkbox" name="" id="" />
+            <p className="text-center text-sm text-gray-400">
+              By signing up, you agree to the our terms and our Privacy Policy
+              agreement.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      </div>
+      <div className="h-[250px] bg-black w-[100%] flex flex-col justify-center items-center">
+        <div className="text-2xl text-white flex justify-center gap-4 w-[100%] ">
+          <div className=" flex justify-center items-center transition-all duration-300 ease-in-out hover:text-[#3c3fde] bg-[#0f0f11] h-12 w-12 rounded-full  ">
+            <FaFacebookF />
+          </div>
+          <div className=" flex justify-center items-center transition-all duration-300 ease-in-out hover:text-[#3c3fde] bg-[#0f0f11]  h-12 w-12 rounded-full  ">
+            <FaXTwitter />
+          </div>
+          <div className=" flex justify-center items-center transition-all duration-300 ease-in-out hover:text-[#3c3fde] bg-[#0f0f11]  h-12 w-12 rounded-full  ">
+            <FaInstagram />
+          </div>
+          <div className=" flex justify-center items-center transition-all duration-300 ease-in-out hover:text-[#3c3fde]  bg-[#0f0f11]  h-12 w-12 rounded-full  ">
+            <FaPinterestP /> 
+          </div>
+        </div>
+           <div className=" text-white flex mt-5 gap-5 font-bold  ">
+            <h1>HOME</h1>
+            <h1>PHONES</h1>
+            <h1>ABOUT</h1>
+            <h1>CONTACTS</h1>
+           </div>
+            <div className="text-gray-500 flex mt-5 gap-1">
+              <p>© 2024 ThemeSphere. Designed by  </p>
+              <p className="text-zinc-400"> ThemeSphere.</p>
+            </div>
+      </div>
+    </>
+  );
+};
+
+export default Footerr;
---
+++ src/component/pages/Homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Navbar from "../../feature/Header/navbar.component";
+const Homepage = () => {
+  return (
+    <nav>
+      <Navbar/>
+    </nav>
+   
+  );
+};
+
+export default Homepage;
